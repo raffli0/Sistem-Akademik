10481d63e7595aecfb7607a8b66088cc8a6f0f59
raffli0 <rafflisaputra123@gmail.com>
2026-04-24 18:55:30 +0700

    readme updatee

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Mahasiswa;
-use Faker\Factory as Faker; // Pastikan baris ini tepat seperti ini
+use Faker\Factory as Faker;
 
 class MahasiswaSeeder extends Seeder
 {
